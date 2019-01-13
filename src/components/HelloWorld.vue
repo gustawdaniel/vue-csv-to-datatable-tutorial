89b991dc846000079002535c6e43ecef12216004
Daniel Gustaw <gustaw.daniel@gmail.com>
2019-01-14 00:51:44 +0100

    Papa parse added, main function achieved

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,12 @@
                     </v-card-title>
 
                     <v-card-actions>
-                        <v-btn block color="success">Select CSV File</v-btn>
+
+                        <v-btn block color="success">
+                            <input type="file" id="csv_upload" name="csv_upload" accept=".csv" class="input-file" style="opacity: 0; display: none">
+                            <label style="width: 100%" for="csv_upload">Select CSV File</label>
+                        </v-btn>
+
                     </v-card-actions>
                 </v-card>
             </v-flex>
@@ -17,7 +22,7 @@
             <v-flex xs12 v-if="headers.length">
                 <v-card hover>
                     <v-card-title>
-                        Data
+                        Data from: <pre class="ml-1">{{fileName}}</pre>
                         <v-spacer></v-spacer>
                         <v-text-field
                                 v-model="search"
@@ -41,15 +46,44 @@
 </template>
 
 <script>
+    import Papa from 'papaparse';
+
     export default {
         data: () => ({
             search: '',
-            headers: [
-                {text: 'Header', value: 'name' }
-            ],
-            items: [
-                { name: 'Value' }
-            ]
-        })
+            headers: [],
+            items: [],
+            fileName: ""
+        }),
+        mounted() {
+            const self = this;
+
+            document.querySelector('#csv_upload').addEventListener('change', (e) => {
+                self.headers = [];
+                self.items = [];
+
+                self.fileName = e.target.files[0].name;
+
+                console.time('parsing');
+
+                Papa.parse(e.target.files[0], {
+                    header: true,
+                    step: function(results) {
+                        if(!self.headers.length) results.meta.fields.forEach((field) => {
+                            self.headers.push({text: field, value: field});
+                        });
+
+                        results.data
+                            .filter(row => results.meta.fields.every(field => row.hasOwnProperty(field)))
+                            .forEach(row => {
+                                self.items.push(row);
+                            })
+                    },
+                    complete: function(results) {
+                        console.timeEnd('parsing');
+                    }
+                });
+            })
+        }
     }
 </script>
